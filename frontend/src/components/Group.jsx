c72094911de9f3265c7b37b47d16a5d152379d15
yosef-mekonin <siyamregnyeshidagna251@gmail.com>
2025-08-16 23:47:32 +0300

    feat: update dependencies and refactor project structure
    - Added new dependencies: @radix-ui/react-dialog, @radix-ui/react-separator, @radix-ui/react-slot, @radix-ui/react-tooltip, class-variance-authority, clsx, express, tailwind-merge, and tailwindcss-animate.
    - Removed unused components: App, Chat, Contact, Navbar, NotFound, and Home.
    - Updated Footer and Group components with new content.
    - Refactored Hero component to include a minimum height.
    - Cleaned up index.css and main.jsx files.
    - Enhanced tailwind.config.js with new theme colors and configurations.
    - Updated vite.config.js to include path aliasing for easier imports.

--- frontend/src/components/Group.jsx
+++ frontend/src/components/Group.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
 
 const Group = () => {
   return (
     <div>
-      
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Non porro autem ex dolorem quaerat dolores tempore at vol
+      uptatum, repellendus dicta asperiores inventore earum cum
+      que voluptatibus impedit modi vel minus magni!
     </div>
   )
 }
