027a9e0058934ec75d8e989cce62987b30e32999
siyamregn777 <yeshidagnasiyamregn@gmail.com>
2025-08-16 23:45:09 +0300

    refactor(Home): remove Navbar component and clean up layout structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
-import Footer from "../components/Footer"
+import Hero from "@/components/Hero"
+import Footer from "@/components/Footer"
 const Home = () => {
   return (
-    <div className="dark">
-      <Navbar />
+    <div >
       <Hero/>
-      <Footer />
+      <Footer/>
     </div>
   )
 }
 
 export default Home
+
