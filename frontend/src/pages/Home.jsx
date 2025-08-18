777a88869c80ae41ff1060433cc091c08c0f00bb
Daniyeshi <yeshidagnadaniel@gmail.com>
2025-08-18 13:21:09 +0300

    Create Home page layout with multiple feature sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
-
-
+import Feature from "../components/Feature"
+import BelowFeature from "../components/BelowFeature"
+import Conversation from "../components/Conversation"
+import Order from "../components/Order"
+import {InfiniteMovingCardsDemo} from "@/components/InifinteCards"
+import Last from "../components/Last"
+import Subscribe from "@/components/Subscribe"
 const Home = () => {
   return (
-    <div className="dark">
-      
+    <div>
       <Hero/>
+      <Feature/>
+      <BelowFeature/>
+      <Conversation/>
+      <Order/>
+      <InfiniteMovingCardsDemo/>
+      <Last/>
+      <Subscribe />
       <Footer />
     </div>
   )
